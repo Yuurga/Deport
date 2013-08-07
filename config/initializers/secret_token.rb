1262ff311d6106a7b6adeb4be58f30c116cfb919
Koroleva Daria <zireaell@land.ru>
2013-08-07 22:58:56 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deport::Application.config.secret_token = 'e9c4ead9b36eb0aa8f69c862cf02ee1f7eb852fae27f2c8fb0d98fabd15603d2d882d3cb4fe49ac8e02a309dfa005c7d2c4d61300439f94a7e2b93e67f0dd002'
